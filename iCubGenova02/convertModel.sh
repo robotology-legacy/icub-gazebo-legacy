29468ee461440fbbcae46f0253fb051054df8ed5
Silvio <pegua1@gmail.com>
2016-09-21 18:32:48 +0200

    Removed iCubGenova02 directory. All data and logic related to model generation has been moved to https://github.com/robotology-playground/icub-model-generator

--- iCubGenova02/convertModel.sh
+++
@@ -1,3 +0,0 @@
-simmechanics_to_urdf iCubGenova02_SIM_MODEL.xml --output xml --yaml iCubGenova02_simmechanics_options.yaml --csv-joints iCubGenova02_joint_parameters.csv --outputfile iCubGenova02.urdf
-simmechanics_to_urdf iCubGenova02_SIM_MODEL.xml --output xml --yaml iCubGenova02NoBattery_simmechanics_options.yaml --csv-joints iCubGenova02_joint_parameters.csv --outputfile iCubGenova02NoBattery.urdf
-gz sdf -p iCubGenova02.urdf > iCubGenova02.sdf
